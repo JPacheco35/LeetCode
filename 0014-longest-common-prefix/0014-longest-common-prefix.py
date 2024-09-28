7eb942f07a456953f81e33979206af2af9ae7661
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-09-28 10:49:36 -0400

    Update 0014-longest-common-prefix.py

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,26 +1,37 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
+        # Case: empty list --> LCP = ""
+        if len(strs) == 0:
+            return ""   
+        
+        # Case: only 1 string in list --> LCP is that string
         if len(strs) == 1:
             return strs[0]
         
+        # Case: there is an empty string in list --> LCP = ""
         for str in strs:
             if str == "":
                 return ""
         
-        res = ""
-        prefix = ""
-        # print(prefix)
+        # start with first char
+        prefix = strs[0][0]
         curLength = 1
+        
+        
         while True:
-            # print("prefix = " + prefix)
+            
+            # check each string of a prefix mismatch --> return previous loop's prefix
+            for str in strs:
+                if str[:curLength] != prefix:
+                    return prefix[:curLength-1]
+                
+            # prefix is the entire first string --> end here, no more characters to pull from
             if prefix == strs[0]:
                 return prefix
             
-            prefix += strs[0][curLength-1]
-            for str in strs:
-                if str[:curLength] != prefix:
-                    return res
-            res = prefix
+            # add next charcter to the prefix
+            prefix += strs[0][curLength]
+
+            # res = prefix
             curLength += 1
-            
