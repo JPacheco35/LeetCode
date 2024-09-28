4b00132d8dd0c63bf0dac4fc25fef1d4c89c88f1
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-09-28 14:35:40 -0400

    Time: 55 ms (51.43%), Space: 16.6 MB (43.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -11,20 +11,16 @@ class Solution:
         num = str(x)
         length = len(num)
         
+        # Case: single digit --> TRUE
         if length == 1:
             return True
         
         if length == 2:
             return num[0] == num[1]
         
-        if length == 3:
-            return num[0] == num[2]
-        
-        # if length == 4:
-        #     return (num[0] == num[3]) and (num[1] == num[2])
-        
-        # if length == 5:
-        #     return (num[0] == num[4]) and (num[1] == num[3])
+        # if length == 3:
+        #     return num[0] == num[2]
+
         
         for i in range(math.floor(length/2)):
             if num[i] != num[length - (i+1)]:
