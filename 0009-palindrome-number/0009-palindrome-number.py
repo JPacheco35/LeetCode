2f5017de276b3cbc43a9050b90f908caca24a34d
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-09-28 14:34:43 -0400

    Time: 47 ms (83.22%), Space: 16.7 MB (7.68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,39 @@
+import math
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        # Case: negative number --> FALSE
+        if x < 0:
+            return False;
+        
+        # convert int into string
+        num = str(x)
+        length = len(num)
+        
+        if length == 1:
+            return True
+        
+        if length == 2:
+            return num[0] == num[1]
+        
+        if length == 3:
+            return num[0] == num[2]
+        
+        # if length == 4:
+        #     return (num[0] == num[3]) and (num[1] == num[2])
+        
+        # if length == 5:
+        #     return (num[0] == num[4]) and (num[1] == num[3])
+        
+        if length % 2 == 0:
+            for i in range(int(length/2)):
+                if num[i] != num[length - (i+1)]:
+                    return False
+                
+        else:
+            for i in range(math.floor(length/2)):
+                if num[i] != num[length - (i+1)]:
+                    return False
+        
+        return True
