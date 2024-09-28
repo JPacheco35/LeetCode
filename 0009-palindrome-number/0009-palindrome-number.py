c389169570a034c957b3d31bc1f82e5faa684888
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-09-28 14:36:27 -0400

    Time: 41 ms (95.09%), Space: 16.5 MB (43.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -10,16 +10,6 @@ class Solution:
         # convert int into string
         num = str(x)
         length = len(num)
-        
-        # Case: single digit --> TRUE
-        # if length == 1:
-        #     return True
-        
-#         if length == 2:
-#             return num[0] == num[1]
-        
-        # if length == 3:
-        #     return num[0] == num[2]
 
         
         for i in range(math.floor(length/2)):
