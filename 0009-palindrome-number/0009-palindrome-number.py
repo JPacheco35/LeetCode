25c6b67c3c4b72662b95c9cb73da325b14cf7801
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-09-28 14:37:37 -0400

    Time: 53 ms (60.09%), Space: 16.6 MB (43.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -11,9 +11,10 @@ class Solution:
         num = str(x)
         length = len(num)
 
-        
+        # check each opposite positon for match, if yes --> FALSE
         for i in range(math.floor(length/2)):
             if num[i] != num[length - (i+1)]:
                 return False
         
+        # if we reach here, no mismatches --> TRUE
         return True
