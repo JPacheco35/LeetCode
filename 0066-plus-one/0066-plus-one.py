4953b8a43b6760ebaaf37f037c54111e297d7ea6
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-10-01 13:01:53 -0400

    Time: 35 ms (67.76%), Space: 16.6 MB (25.71%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,19 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-        # begin with carry_over at 1 because we are adding 1 to the original number
-        carry_over = 1
-        
         # traverse array from back to front        
         for num in reversed(range(len(digits))):
             
-            # Case: current digit is 9 --> set digit to 0, carry_over remains 1
+            # Case: current digit is 9 --> set digit to 0, carry over 1 to next position
             if digits[num] == 9:
                 digits[num] = 0
                 
-            # Otherwise: just add carry_over to digit, end here because there is no more overflow
+            # Otherwise: just add 1 to digit, end here because there is no more overflow
             else:
-                digits[num] += carry_over
+                digits[num] += 1
                 return digits
         
         # if we reach here, need to add a new digit space (ie. 9 + 1 = 10)
