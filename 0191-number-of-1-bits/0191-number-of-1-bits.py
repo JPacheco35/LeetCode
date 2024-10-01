e33b6b2a07161eb738f4365d607a0b75a378e6c4
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-10-01 13:31:42 -0400

    Time: 34 ms (68.70%), Space: 16.4 MB (94.41%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,12 +1,16 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         
-        s = str(bin(n))[2:]
+        # store # of 1 bits
+        count = 0
         
-        nums = [int(char) for char in s]
-        
-        total = 0
-        for num in nums:
-            total += num
-        
-        return total
+        # while n > 0
+        while n > 0:
+            
+            # Case: rightmost bit (LSB) is 1
+            count += n&1
+            
+            # shift n to the right 1 bit to check next bit
+            n = n >> 1
+                
+        return count
