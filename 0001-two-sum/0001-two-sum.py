3c11c82bc637d7f9ad3721dbcc13632c475492a6
John Pacheco <99419989+JPacheco35@users.noreply.github.com>
2024-10-09 17:29:29 -0400

    Time: 3954 ms (5.00%), Space: 17.4 MB (64.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
+        
+        length = len(nums)
+        
+        # loop thorugh each possible pairing --> o(n^2)
+        for i in range(length):
+            for j in range(length):
+                
+                # can't add same num twice 
                 if i == j :
                     continue
+                    
+                # if two num add to target, return indices
                 if nums[i] + nums[j] == target:
                     return [i,j]
                 
-        
+        # we should never reach here
+        return -1
